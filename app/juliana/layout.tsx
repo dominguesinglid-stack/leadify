c39df120a494922543f764642b6412c77fafcf9e
Inglid Domingues <dominguesinglid@gmail.com>
2026-06-21 13:25:53 -0300

    ajusta hero mobile h1

---
+++ app/juliana/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Juliana Ottero | Personal Trainer",
+  description:
+    "Consultoria online e presencial com acompanhamento personalizado",
+};
+
+export default function JulianaLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
